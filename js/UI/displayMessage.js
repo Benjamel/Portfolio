3e4ebe6dded07f632474d2eeb46bb34ebfcb2eb8
Benjamel <benjamin.meldal@gmail.com>
2022-12-18 21:24:14 +0100

    test

--- js/UI/displayMessage.js
+++
@@ -1,4 +0,0 @@
-export function displayMessage(messageType, message, target) {
-    const element = document.querySelector(target);
-    element.innerHTML = `<div class="message ${messageType}">${message}</div>`;
-}
